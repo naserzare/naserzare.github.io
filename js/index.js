d62ff1b11b53b948f7422da47399ec698e7e107b
naserzare <naserzare@live.com>
2022-05-23 15:56:51 +0430

    asd

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-// Assignment
-let data1 = 10;
-console.log(data1);
+let aliw = 78;
+let alih = 1.69;
+let rezaw = 92;
+let rezah = 1.95;
+let abmi = aliw / alih ** 2;
+let rbmi = rezaw / (rezah * rezah);
+if (abmi > rbmi) {
+    alert("Ali's BMI is Higher Than Reza");
+    alert(`Ali's BMI (${abmi}) Is Higher Than Reza's BMI (${rbmi})`);
+} else {
+    alert("Reza's BMI is Higher Than Ali");
+    alert(`Reza's BMI (${abmi}) Is Higher Than Ali's BMI (${rbmi})`);
+}
